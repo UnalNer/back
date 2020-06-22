7e9b907c2710d8b70d915e18242a126e057d34c0
José Nieto <jdnv.123@gmail.com>
2020-06-22 15:41:38 -0500

    Convert to API

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return 'Hello world!'
 
 if __name__ == "__main__":
     app.run(debug=True)
